537f9de5c464bb5f38dbc54b5998b5bc2fc7719c
Andrew <a9507652513@gmail.com>
2024-12-24 20:37:34 +0300

    rsyfjfgyj

---
+++ main.py
@@ -0,0 +1,17 @@
+def add_element(new: list[str] | str, src: list = []) -> list:
+    if isinstance(new, list):
+        src.extend(new)
+    else:
+        src.append(new)
+    return src
+
+
+if __name__ == "__main__":
+    print("hello")
+    value1 = add_element("test")
+    print(value1)
+    value2 = add_element(["test2", "test3"])
+    print(value2)
+    print("heaewrfawrfllo")
+    value3 = add_element(["test4"], value2)
+    print(value3)
